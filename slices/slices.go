57a1ab35f91b350c3dfaf2b2a9bde30be3b646cc
d-smith <doug.smith.mail@gmail.com>
2015-08-22 06:29:02 -0600

    converted all functions to methods with GenericSlice receiver

--- slices/slices.go
+++ slices/slices.go
@@ -4,21 +4,9 @@ import (
 	"errors"
 )
 
-
-
 type GenericSlice []interface{}
 
-func(s GenericSlice)  Last()(interface{}, error) {
-	length := len(s)
-	if length == 0 {
-		return -1, errors.New("Can't return last element from zero length slice")
-	}
-
-	return s[length-1], nil
-}
-
-//Last returns the last element of a non-empty slice, or an error if given an empty slice.
-func Last(s []interface{}) (interface{}, error) {
+func (s GenericSlice) Last() (interface{}, error) {
 	length := len(s)
 	if length == 0 {
 		return -1, errors.New("Can't return last element from zero length slice")
@@ -28,19 +16,20 @@ func Last(s []interface{}) (interface{}, error) {
 }
 
 //Penultimate returns the next to last element of a non-empty slice
-func Penultimate(s []interface{}) (interface{}, error) {
-	length := len(s); if length < 2 {
+func (s GenericSlice) Penultimate() (interface{}, error) {
+	length := len(s)
+	if length < 2 {
 		return -1, errors.New("Can't return next to last element from slice of length less than two.")
 	}
 
-	return s[length - 2],nil
+	return s[length-2], nil
 }
 
 //Kth returns the kth element of a slice, starting with 0. Yah, trivial stuff when using slices...
-func Kth(k int, s []interface{})(interface{}, error) {
-	if k < 0 || k > len(s) - 1 {
+func (s GenericSlice) Kth(k int) (interface{}, error) {
+	if k < 0 || k > len(s)-1 {
 		return -1, errors.New("k index is out of bounds")
 	}
 
-	return s[k],nil
+	return s[k], nil
 }
--- slices/slices_test.go
+++ slices/slices_test.go
@@ -1,68 +1,73 @@
 package slices
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestLast(t *testing.T) {
 	t.Log("given a slice of integers")
-	s := []interface{}{1, 2, 3, 4, 5}
+	s := GenericSlice{1, 2, 3, 4, 5}
 
 	t.Log("when last is called, the last element of the slice is returned")
-	last, err := Last(s)
+	last, err := s.Last()
 
-	assert.Nil(t,err)
-	assert.Equal(t,5,last)
+	assert.Nil(t, err)
+	assert.Equal(t, 5, last)
 
-	a,_ := Last(s)
-	assert.Equal(t, 10, a.(int) + a.(int))
+	a, _ := s.Last()
+	assert.Equal(t, 10, a.(int)+a.(int))
 }
 
 func TestGenericLast(t *testing.T) {
-	s := GenericSlice{1,2,3,4,5}
-	last, err := Last(s)
+	s := GenericSlice{1, 2, 3, 4, 5}
+	last, err := s.Last()
 
-	assert.Nil(t,err)
-	assert.Equal(t,5,last)
+	assert.Nil(t, err)
+	assert.Equal(t, 5, last)
 }
 
 func TestLastErrCheck(t *testing.T) {
 	t.Log("Given an empty slice")
-	var s []interface{}
+	var s GenericSlice
 
 	t.Log("when passed to last, an error is returned")
-	_,err := Last(s)
-	assert.NotNil(t,err)
+	_, err := s.Last()
+	assert.NotNil(t, err)
 }
 
 func TestPenultimate(t *testing.T) {
 	t.Log("Given a slice of 2 or more integers")
-	s := []interface{}{1,2,3,4,5}
+	s := GenericSlice{1, 2, 3, 4, 5}
 
 	t.Log("when Penultimate is called, the next to last element is returned")
-	n, err := Penultimate(s)
-	assert.Nil(t,err)
-	assert.Equal(t,4,n.(int))
+	n, err := s.Penultimate()
+	assert.Nil(t, err)
+	assert.Equal(t, 4, n.(int))
 }
 
 func TestKth(t *testing.T) {
-	s := []interface{}{1,2,3,4,5}
+	s := GenericSlice{1, 2, 3, 4, 5}
 
 	var v interface{}
 	var err error
 
-	v,err = Kth(-1, s)
+	v, err = s.Kth(-1)
 	assert.NotNil(t, err)
 
-	v,err = Kth(300, s)
+	v, err = s.Kth(5)
 	assert.NotNil(t, err)
 
-	v,err = Kth(0, s)
-	assert.Nil(t,err)
+	v, err = s.Kth(0)
+	assert.Nil(t, err)
 	assert.Equal(t, 1, v.(int))
 
-	v,err = Kth(4, s)
-	assert.Nil(t,err)
+	v, err = s.Kth(4)
+	assert.Nil(t, err)
 	assert.Equal(t, 5, v.(int))
+
+	s = append(s, 6)
+	v, err = s.Kth(5)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, v.(int))
 }
